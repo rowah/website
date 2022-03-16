9f959cd4f4d4cb5f14daef9bfb7a5bf301d704a2
James Rowa <76947107+rowah@users.noreply.github.com>
2022-03-16 22:38:54 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>JRowah...</h1>
+    <h1>JRowah</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
